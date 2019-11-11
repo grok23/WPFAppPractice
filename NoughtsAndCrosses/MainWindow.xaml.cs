c2fddd6d2774e3f9ec1133718e12b20ef9510ed1
grok23 <46896171+grok23@users.noreply.github.com>
2019-11-11 23:55:29 +0000

    tidying

--- NoughtsAndCrosses/MainWindow.xaml.cs
+++ NoughtsAndCrosses/MainWindow.xaml.cs
@@ -163,7 +163,7 @@ namespace NoughtsAndCrosses
             }      
         }
 
-        private static int CheckWinState(string[,] board)                //a method to check for a win state
+        private static int CheckWinState(string[,] board)               //a method to check for a win state
 
         {
             if ((board[0, 0] == board[0, 1] && board[0, 1] == board[0, 2]) || (board[1, 0] == board[1, 1] && board[1, 1] == board[1, 2]) || (board[2, 0] == board[2, 1] && board[2, 1] == board[2, 2])) //winning condition for horizontal rows (is element a the same as element b and is b the same as c)
@@ -201,7 +201,7 @@ namespace NoughtsAndCrosses
             txtBlk9.Text = board[2, 2];
         }
 
-        private string[,] BoardReset()                                   //method to clone the boardReset array, (stops the board reset function only working once)
+        private string[,] BoardReset()                                  //method to clone the boardReset array, (stops the board reset function only working once)
         {
             return (string[,])boardReset.Clone();                       //clones the array to stop the original being altered
         }
