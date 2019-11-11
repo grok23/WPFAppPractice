030e89ead1411eabb7dcda243764761e98aa9415
grok23 <46896171+grok23@users.noreply.github.com>
2019-11-11 20:18:43 +0000

    tried adding key press event handler for enter key to textbox and failed

--- NoughtsAndCrosses/MainWindow.xaml.cs
+++ NoughtsAndCrosses/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -46,9 +47,17 @@ namespace NoughtsAndCrosses
         {
             txtBlkEnterMove.Text = "";
         }
+
+        private void NmbrValidation(object sender, TextCompositionEventArgs e)  //restricts characters entered into the text box to digits 1-9
+        {
+            Regex regex = new Regex("[^1-9]");
+            e.Handled = regex.IsMatch(e.Text);
+        }
         private void btnMove_Click(object sender, RoutedEventArgs e)
         {
-            if (player % 2 != 0)                                        //if to check whose turn it is, a remainder of 0  = player 2, else player 1
+            
+            move = 0;
+            if (player % 2 != 0)                                        //if to check whose turn it is
             {
                 txtBlkGameInfo.Background = Brushes.White;
                 txtBlkGameInfo.Text = "Player 2, it's your turn to pick a square.";
@@ -58,63 +67,78 @@ namespace NoughtsAndCrosses
                 txtBlkGameInfo.Background = Brushes.White;
                 txtBlkGameInfo.Text = "Player 1, it's your turn to pick a square.";
             }
+            if (txtBlkEnterMove.Text != "")                             //checks that the textbox has a value in it
+            {
+                move = int.Parse(txtBlkEnterMove.Text);                 //value enters the switch statement
+                switch (move)                                           //switch for the move variable, allows for translation of the player input of a single int to be translated to a pair of array indices 
+                {
+                    case 1:                                             //position 1 = 0,0 in the array, position 2 is 0,1 etc. this carries on till position 9 on the board which is 2,2
+                        row = 0; column = 0; break;
+                    case 2:
+                        row = 0; column = 1; break;
+                    case 3:
+                        row = 0; column = 2; break;
+                    case 4:
+                        row = 1; column = 0; break;
+                    case 5:
+                        row = 1; column = 1; break;
+                    case 6:
+                        row = 1; column = 2; break;
+                    case 7:
+                        row = 2; column = 0; break;
+                    case 8:
+                        row = 2; column = 1; break;
+                    case 9:
+                        row = 2; column = 2; break;
+                }
+                if (board[row, column] != "X" && board[row, column] != "O") //checks that the point on the board is clear  
+                {
+                    if (player % 2 != 0)                                //if it's player 1's turn put an X in the array
+                    {
+                        board[row, column] = "X";                       //updates the board array on player 1 turns 
+                        player++;                                       //adds 1 to the player variable at the end of a successful turn
+                    }
+                    else                                                //if it's player 2's turn put an O in the array
+                    {
+                        board[row, column] = "O";                       //updates the board array on player 2 turns 
+                        player++;                                       //adds 1 to the player variable at the end of a successful turn
+                    }
+                }
+                else                                                    //checks if a position is already occupied and asks them to try again if it is
+                {
+                    txtBlkGameInfo.Background = Brushes.Red;
+                    txtBlkGameInfo.Text = $"Sorry, but position {move} is already occupied by an {board[row, column]}. Try another move.";
+                }
 
-            move = int.Parse(txtBlkEnterMove.Text);                     //takes user input for their move as a single int
-            switch (move)                                               //switch for the move variable, allows for translation of the player input of a single int to be translated to a pair of array indices 
-            {
-                case 1:                                                 //position 1 = 0,0 in the array, position 2 is 0,1 etc. this carries on till position 9 on the board which is 2,2
-                    row = 0; column = 0; break;
-                case 2:
-                    row = 0; column = 1; break;
-                case 3:
-                    row = 0; column = 2; break;
-                case 4:
-                    row = 1; column = 0; break;
-                case 5:
-                    row = 1; column = 1; break;
-                case 6:
-                    row = 1; column = 2; break;
-                case 7:
-                    row = 2; column = 0; break;
-                case 8:
-                    row = 2; column = 1; break;
-                case 9:
-                    row = 2; column = 2; break;
-            }
-            if (board[row, column] != "X" && board[row, column] != "O") //checks that the point on the board is clear  
-            {
-                if (player % 2 != 0)                                    //if it's player 1's turn put an X in the array
+                DisplayBoard();                                         //output the updated board 
+                winState = CheckWinState(board);                        //calls the CheckWinState method to update the winState variable
+
+                if (winState == 1)                                      //if winState = 1 then we have a winner
                 {
-                    board[row, column] = "X";                           //updates the board array on player 1 turns 
-                    player++;                                           //adds 1 to the player variable at the end of a successful turn
+                    txtBlkGameInfo.Background = Brushes.Gold;
+                    txtBlkGameInfo.Text = $"Player {(player % 2) + 1} is the winner.";//, (player % 2) + 1; //winning player number is found by looking for a remainder again and adding 1 to correct the off by 1 error    
+                    Keyboard.Focus(btnResetBoard);                      //sets focus on the reset board button
                 }
-                else                                                    //if it's player 2's turn put an O in the array
+                if (winState == 2)                                      //if winState = 2 the match is a draw
                 {
-                    board[row, column] = "O";                           //updates the board array on player 2 turns 
-                    player++;                                           //adds 1 to the player variable at the end of a successful turn
+                    txtBlkGameInfo.Background = Brushes.White;
+                    txtBlkGameInfo.Text = "The game was a draw";
+                    Keyboard.Focus(btnResetBoard);
+                }
+                else if (winState == 0)
+                {
+                    txtBlkEnterMove.Text = "";                          //clear text box ready for next move
+                    Keyboard.Focus(txtBlkEnterMove);                    //sets focus back on the text box again so that players don't need to click on it each time
                 }
             }
-            else                                                        //checks if a position is already occupied and asks them to try again if it is
+            else if (txtBlkEnterMove.Text == "")                        //in the case of no value being entered when button is clicked, prompts user to enter a valid one
             {
                 txtBlkGameInfo.Background = Brushes.Red;
-                txtBlkGameInfo.Text = $"Sorry, but position {move} is already occupied by an {board[row, column]}. Try another move.";
+                txtBlkGameInfo.Text = "Please choose a valid move";
+                txtBlkEnterMove.Text = "";                              //clear text box ready for next move
+                Keyboard.Focus(txtBlkEnterMove);
             }
-
-            DisplayBoard();                                             //output the updated board 
-            winState = CheckWinState(board);                            //calls the CheckWinState method to update the winState variable
-
-            if (winState == 1)                                          //if winState = 1 then we have a winner
-            {
-                txtBlkGameInfo.Background = Brushes.Gold;
-                txtBlkGameInfo.Text = $"Player {(player % 2) + 1} is the winner.";//, (player % 2) + 1; //winning player number is found by looking for a remainder again and adding 1 to correct the off by 1 error    
-            }
-            if (winState == 2)                                          //if winState = 2 the match is a draw
-            {
-                txtBlkGameInfo.Background = Brushes.White;
-                txtBlkGameInfo.Text = "The game was a draw";
-            }
-            txtBlkEnterMove.Text = "";                                  //clear text box ready for next move
-            Keyboard.Focus(txtBlkEnterMove);                            //sets focus back on the text box again so that players don't need to click on it each time
+            
         }
         
         public static int CheckWinState(string[,] board)                //a method to check for a win state
@@ -141,13 +165,7 @@ namespace NoughtsAndCrosses
                 return 0;                                               //if no win state is met and the match hasn't reached a stalemate yet, then return 0 as the game is still in progress
             }
         }
-        /*
-        private void btnMove_ClickBoardReset(object sender, RoutedEventArgs e)  //trying to build a board reset function into the game
-        {
-            InitializeComponent();
-        }
-        */
-
+        
         private void btnResetBoard_Click(object sender, RoutedEventArgs e)
         {
             //reset all the variables
@@ -155,12 +173,14 @@ namespace NoughtsAndCrosses
             winState = 0;
             row = 0;                                                    
             column = 0;                                                 
-            player = 1;                                                 
+            player = 1;
+            txtBlkEnterMove.Text = "Choose your move";
+            Keyboard.Focus(txtBlkEnterMove);
             DisplayBoard();                                             //call displayboard to show the reset board
             txtBlkGameInfo.Background = Brushes.White;
             txtBlkGameInfo.Text = "Playing again? Player one will be X's and player 2 will play O's.";
-            
         }
+
         private void DisplayBoard()
         {
             txtBlk1.Text = board[0, 0];
@@ -172,6 +192,6 @@ namespace NoughtsAndCrosses
             txtBlk7.Text = board[2, 0];
             txtBlk8.Text = board[2, 1];
             txtBlk9.Text = board[2, 2];
-        }
+        }        
     }
 }
